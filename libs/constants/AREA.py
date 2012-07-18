cbc0f8e04de1de815a3f66c4c2a3991998eeb36c
jiaxiaolei <jiaxiaolei@fbfde4e3-4bc3-4d81-a63b-7b332075e0cf>
2012-07-18 07:23:33 +0000

    import area.
    git-svn-id: http://www.ichebao.net/svn/acb/trunk@11 fbfde4e3-4bc3-4d81-a63b-7b332075e0cf

---
+++ libs/constants/AREA.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from utils.dotdict import DotDict
+
+CATEGORY = DotDict(PROVINCE=10,
+                   CITY=20)
+
+# if you can work on all provinces, set area_id to 0.
+ALL_PROVINCES = 0
+
+# province id for every province
+PROVINCE = DotDict(LIAONING=240)
