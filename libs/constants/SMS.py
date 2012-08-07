f99d7226456cdffde16479478305cec04ce20ee0
liushijia <liushijia@fbfde4e3-4bc3-4d81-a63b-7b332075e0cf>
2012-08-07 03:34:23 +0000

    additional document
    git-svn-id: http://www.ichebao.net/svn/acb/trunk@99 fbfde4e3-4bc3-4d81-a63b-7b332075e0cf

--- libs/constants/SMS.py
+++ libs/constants/SMS.py
@@ -2,8 +2,6 @@
 
 from utils.dotdict import DotDict
 
-CATEGORY = DotDict(RECEIVE=10,
-                   SEND=20)
 
 SMS_EXPIRY=24*60*60
 
@@ -11,3 +9,15 @@ LQ = DotDict(WEB=30,
              SMS=10)
 
 LQ_INTERVAL = 3 * 60 # 3mins
+
+CATEGORY = DotDict(RECEIVE=10,
+                   SEND=20)
+
+SENDSTATUS = DotDict(PENDING=0,
+                     SUCCESS=1,
+                     FAILURE=2)
+
+USERSTATUS = DotDict(NOSYNC=-1,
+                     SUCCESS=0,
+                     FAILURE=1)
+
