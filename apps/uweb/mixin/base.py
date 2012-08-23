521fb40ca0c24c20eb70f8a3a0ff91dfb20cef42
jiaxiaolei <jiaxiaolei@fbfde4e3-4bc3-4d81-a63b-7b332075e0cf>
2012-08-23 03:02:40 +0000

    -- bug fixed. use SMSCode.SMS_LQ
    git-svn-id: http://www.ichebao.net/svn/acb/trunk@152 fbfde4e3-4bc3-4d81-a63b-7b332075e0cf

--- apps/uweb/mixin/base.py
+++ apps/uweb/mixin/base.py
@@ -6,6 +6,7 @@ from utils.misc import get_lq_sms_key
 from helpers.smshelper import SMSHelper
 
 from constants import SMS
+from codes.smscode import SMSCode
 
 class BaseMixin(object):
 
@@ -14,7 +15,7 @@ class BaseMixin(object):
         """
         lq_sms_key = get_lq_sms_key(sim) 
         if not self.redis.getvalue(lq_sms_key): 
-            sms = "LQ %s" % interval 
+            sms = SMSCode.SMS_LQ % interval 
             SMSHelper.send(sim, sms) 
             logging.info("Send %s to Sim: %s", sms, sim) 
             self.redis.setvalue(lq_sms_key, True, SMS.LQ_INTERVAL)
