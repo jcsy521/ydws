ef134fbbd369c02d22ec3c2115244733b2c14101
jiaxiaolei <jiaxiaolei@fbfde4e3-4bc3-4d81-a63b-7b332075e0cf>
2012-09-01 02:58:50 +0000

    -- add lq_sms in switchcar
    git-svn-id: http://www.ichebao.net/svn/acb/trunk@287 fbfde4e3-4bc3-4d81-a63b-7b332075e0cf

--- apps/uweb/handlers/car.py
+++ apps/uweb/handlers/car.py
@@ -9,10 +9,11 @@ from utils.misc import DUMMY_IDS
 from utils.dotdict import DotDict
 from codes.errorcode import ErrorCode
 from constants import UWEB, SMS
+from mixin.base import  BaseMixin
 from base import BaseHandler, authenticated
 
        
-class SwitchCarHandler(BaseHandler):
+class SwitchCarHandler(BaseHandler, BaseMixin):
     """Switch current car for the current user.
     """
     @authenticated
@@ -26,28 +27,13 @@ class SwitchCarHandler(BaseHandler):
                                   "  LIMIT 1",
                                   tid)
             if monitor: 
-#                location = self.db.get("SELECT speed, timestamp, category, name"
-#                                       "  FROM T_LOCATION"
-#                                       "  WHERE tid = %s"
-#                                       "    ORDER BY timestamp DESC"
-#                                       "    LIMIT 1",
-#                                       monitor.tid)
+                self.send_lq_sms(self.current_user.sim, SMS.LQ.WEB)
+                
                 self.bookkeep(dict(uid=self.current_user.uid,
                                    tid=tid,
                                    sim=monitor.sim))
                 status = ErrorCode.SUCCESS
-                # NOTE: if there is no records in T_LOCATION, when the car
-                # has never got location, just return None
                 self.write_ret(status) 
-#                self.write_ret(status, 
-#                    dict_=DotDict(car_info=DotDict(tid=monitor.tid,
-#                                                   defend_status=monitor.defend_status,
-#                                                   timestamp=location.timestamp if location else None,
-#                                                   speed=location.speed if location else 0,
-#                                                   event_status=location.category if location else 0,
-#                                                   address=location.name if location else None,
-#                                                   login=monitor.login,
-#                                                   lock_status=monitor.lock_status)))
             else:
                 status = ErrorCode.NO_TERMINAL
                 self.write_ret(status) 
