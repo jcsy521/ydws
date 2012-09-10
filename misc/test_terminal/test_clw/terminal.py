1fb5cfbc96a59b61d0a16d99c6da2f9bdbca0707
guozhaoxia <guozhaoxia@fbfde4e3-4bc3-4d81-a63b-7b332075e0cf>
2012-09-10 10:23:30 +0000

    使用新协议的模拟终端
    git-svn-id: http://www.ichebao.net/svn/acb/trunk@322 fbfde4e3-4bc3-4d81-a63b-7b332075e0cf

--- misc/test_terminal/test_clw/terminal.py
+++ misc/test_terminal/test_clw/terminal.py
@@ -11,25 +11,27 @@ from base import BaseParser
 
 class Terminal(object):
 
-    login_mg = "[%s,,1,1.0.0,A123,T1,15002494865,13011292217,460023615223538,355889008722099,CLW]"
-    heartbeat_mg = "[%s,%s,1,1.0.0,A123,T2,23:9:95]"
-    realtime_mg = "[%s,%s,1,1.0.0,A123,T4,1,E,113.252432,N,22.564152,120.3,270.5,1,460:00:10101:03633,23:9:100,%s]"
+    login_mg = "[%s,,1,1.0.0,B123,T1,13688888888,15901258591,888823615223538,8888889008722099,CLW,0,]"
+    heartbeat_mg = "[%s,%s,1,1.0.0,B123,T2,23:9:95]"
+    realtime_mg = "[%s,%s,1,1.0.0,B123,T4,1,E,113.252432,N,22.564152,120.3,270.5,1,460:00:10101:03633,23:9:100,%s]"
 
-    location_mg = "[%s,%s,1,1.0.0,A123,T3,0,E,113.25,N,22.564152,120.3,270.5,1,460:00:10101:03633,23:9:100,%s]"
-    query_args_mg = "[%s,%s,1,1.0.0,A123,T5,%s]"
-    set_args_mg = "[%s,%s,1,1.0.0,A123,T6,%s]"
-    reboot_mg = "[%s,%s,1,1.0.0,A123,T7,1]"
-    remote_set_mg = "[%s,%s,1,1.0.0,A123,T8,1]"
-    remote_unset_mg = "[%s,%s,1,1.0.0,A123,T9,1]"
-    locationdesc_mg = "[%s,%s,1,1.0.0,A123,T10,E,113.252432,N,22.564152]"
-    pvt_mg = "[%s,%s,1,1.0.0,A123,T11,{E,113.252432,N,22.564152,120.3,270.5,1343278800},{E,114.252432,N,23.564152,130.3,280.5,1343279800}]"
-    charge_mg = "[%s,%s,1,1.0.0,A123,T12,%s]"
-    move_report_mg = "[%s,%s,1,1.0.0,A123,T13,2,E,113.2524,N,22.5641,120.3,270.5,1,460:00:10101:03633,23:9:100,%s,1]"
-    poweroff_report_mg = "[%s,%s,1,1.0.0,A123,T15,1,E,113.252,N,22.564,120.3,270.5,1,460:00:10101:03633,23:9:100,%s,1]"
-    powerlow_report_mg = "[%s,%s,1,1.0.0,A123,T14,0,E,113.252,N,22.564,120.3,270.5,1,460:00:10101:03633,23:9:20,%s,1]"
-    sos_report_mg = "[%s,%s,1,1.0.0,A123,T16,2,E,113.2524,N,22.5641,120.3,270.5,1,460:00:10101:03633,23:9:100,%s,1]" 
+    location_mg = "[%s,%s,1,1.0.0,B123,T3,0,E,113.25,N,22.564152,120.3,270.5,1,460:00:10101:03633,23:9:100,%s]"
+    query_args_mg = "[%s,%s,1,1.0.0,B123,T5,%s]"
+    set_args_mg = "[%s,%s,1,1.0.0,B123,T6,%s]"
+    reboot_mg = "[%s,%s,1,1.0.0,B123,T7,1]"
+    remote_set_mg = "[%s,%s,1,1.0.0,B123,T8,1]"
+    remote_unset_mg = "[%s,%s,1,1.0.0,B123,T9,0]"
+    locationdesc_mg = "[%s,%s,1,1.0.0,B123,T10,E,113.252432,N,22.564152,0,460:0:4489:25196]"
+    pvt_mg = "[%s,%s,1,1.0.0,B123,T11,{E,113.252432,N,22.564152,120.3,270.5,1343278800},{E,114.252432,N,23.564152,130.3,280.5,1343279800}]"
+    charge_mg = "[%s,%s,1,1.0.0,B123,T12,%s]"
+    move_report_mg = "[%s,%s,1,1.0.0,B123,T13,2,E,113.2524,N,22.5641,120.3,270.5,1,460:00:10101:03633,23:9:100,%s,1]"
+    poweroff_report_mg = "[%s,%s,1,1.0.0,B123,T15,1,E,113.252,N,22.564,120.3,270.5,1,460:00:10101:03633,23:9:100,%s,1]"
+    powerlow_report_mg = "[%s,%s,1,1.0.0,B123,T14,0,E,113.252,N,22.564,120.3,270.5,1,460:00:10101:03633,23:9:20,%s,1]"
+    sos_report_mg = "[%s,%s,1,1.0.0,B123,T16,2,E,113.2524,N,22.5641,120.3,270.5,1,460:00:10101:03633,23:9:100,%s,1]" 
     #speed_report_mg = "[%s,1,V1.0.0,A123045612AA123487,T15,1,E,113.2524,N,22.5641,120.3,270.5,1, 460:00:16662:40953]"
     #mileage_count_mg = "[%s, , ,A123045612AA123487,T16,2000]"
+    config_mg = "[%s,%s,1,1.0.0,B123,T17]"
+    defend_report_mg = "[%s,%s,1,1.0.0,B123,T18,1]"
 
     ARGS = DotDict(PSW="123456",
                    GSM=16,
@@ -41,8 +43,8 @@ class Terminal(object):
                    SERVICE_STATUS=0,
                    TRACE=0,
                    PBAT=80,
-                   MOBILE="15002494865",
-                   PARENT_MOBILE="13011292217",
+                   MOBILE="15901258591",
+                   PARENT_MOBILE="13693675352",
                    RADIUS=300,
                    VIB=1,
                    VIBL=7,
@@ -54,8 +56,8 @@ class Terminal(object):
                    VBAT='3713300:4960750:303500',
                    VIN='11145600',
                    PLCID='123',
-                   IMSI='460023615223538',
-                   IMEI='355889008722099'
+                   IMSI='960023615223538',
+                   IMEI='955889008722099'
                    )
 
     def __init__(self):
@@ -73,6 +75,10 @@ class Terminal(object):
         info = bp.packet_info 
         self.sessionID = info[3]
         self.logging.info("login success!")
+        t_time = int(time.time())
+        mg = self.config_mg% (t_time, self.sessionID)
+        self.socket.send(mg)
+        print 'send config: ', mg
 
     def heartbeat_response(self):
         self.logging.info("heartbeat send success!")
@@ -81,11 +87,11 @@ class Terminal(object):
         self.logging.info("location upload success!")
 
     def heartbeat(self):
-        time.sleep(2)
+        time.sleep(30)
         while True:
             self.logging.info("Send heartbeat:\n%s", self.heartbeat_mg)
             self.socket.send(self.heartbeat_mg % (int(time.time()), self.sessionID))
-            time.sleep(120)
+            time.sleep(30)
     
     def realtime(self):
         #t_time = time.strftime("%Y-%m-%d %H:%M:%S")
@@ -104,12 +110,15 @@ class Terminal(object):
             time.sleep(60)
  
     def report_once_mg(self):
-        time.sleep(30)
+        #time.sleep(30)
         s = u'查询余额服务：您好，您的总账户余额为343.91元，感谢您的使用'
         t_time = int(time.time())
         charge = base64.b64encode(s.encode("utf-8", 'ignore'))
         charge_mg = self.charge_mg % (t_time, self.sessionID, charge)
-        mgs = [(self.locationdesc_mg % (t_time, self.sessionID)),(self.pvt_mg % (t_time, self.sessionID)), charge_mg] 
+        pvt_mg = self.pvt_mg % (t_time, self.sessionID)
+        locationdesc_mg = self.locationdesc_mg % (t_time, self.sessionID)
+        defend_status_mg = self.defend_status_mg % (t_time, self.sessionID)
+        mgs = [charge_mg, pvt_mg, locationdesc_mg, defend_status_mg]
         for mg in mgs:
             self.logging.info("Send report mg:\n%s", mg)
             self.socket.send(mg)
@@ -181,6 +190,12 @@ class Terminal(object):
         desc = bp.packet_info[-1:][0]
         locationdesc = base64.b64decode(desc)
         locationdesc = locationdesc.decode("utf-8")
+    def config(self, bp):
+        print 'config: ', bp
+        self.logging.info("Report config success!")
+    def defend_status(self, bp):
+        print 'defend status: ', bp
+        self.logging.info("Report defend_status success!")
 
     def pvt(self):
         self.logging.info("Report pvt success!")
@@ -224,8 +239,10 @@ class Terminal(object):
             self.charge()
         elif bp.type in ("S13, S14, S15, S16"):
             self.async()
-        #elif bp.type == "S16":
-        #    self.mileage_count()
+        elif bp.type == "S17":
+            self.config()
+        elif bp.type == "S18":
+            self.defend_status()
     
     def tcpClient(self):
         try:
