77ca834dc143bf3d9b07c2c1ff0365b5557911c1
guozhaoxia <guozhaoxia@fbfde4e3-4bc3-4d81-a63b-7b332075e0cf>
2012-09-18 08:53:53 +0000

    处理&
    git-svn-id: http://www.ichebao.net/svn/acb/trunk@367 fbfde4e3-4bc3-4d81-a63b-7b332075e0cf

--- apps/eventer/handlers/packettask.py
+++ apps/eventer/handlers/packettask.py
@@ -184,8 +184,12 @@ class PacketTask(object):
                 pass
 
             if report.lon and report.lat:
-                wap_url = 'http://api.map.baidu.com/staticimage?center=%s,%s%26width=800%26height=800%26zoom=17%26markers=%s,%s'
-                wap_url = wap_url % (report.lon/3600000.0, report.lat/3600000.0, report.lon/3600000.0, report.lat/3600000.0)
+                #wap_url = 'http://api.map.baidu.com/staticimage?center=%s,%s%26width=800%26height=800%26zoom=17%26markers=%s,%s'
+                #wap_url = wap_url % (report.lon/3600000.0, report.lat/3600000.0, report.lon/3600000.0, report.lat/3600000.0)
+                wap_url = 'http://api.map.baidu.com/staticimage?center=' +\
+                          str(report.lon/3600000.0) + ',' + str(report.lat/3600000.0) +\
+                          '%26width=800%26height=800%26zoom=17%26markers=' +\
+                          str(report.lon/3600000.0) + ',' + str(report.lat/3600000.0)
                 sms += u"点击" + wap_url
 
             self.sms_to_user(report.dev_id, sms, user)
