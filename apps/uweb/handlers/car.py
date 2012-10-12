a95a021952cf1fa057c64d7908fb7e172c8a6e94
guozhaoxia <guozhaoxia@fbfde4e3-4bc3-4d81-a63b-7b332075e0cf>
2012-10-12 06:34:22 +0000

    切换车辆3min后平台主动查询终端挂件列表
    git-svn-id: http://www.ichebao.net/svn/acb/trunk@486 fbfde4e3-4bc3-4d81-a63b-7b332075e0cf

--- apps/uweb/handlers/car.py
+++ apps/uweb/handlers/car.py
@@ -1,13 +1,19 @@
 # -*- coding: utf-8 -*-
 
 import logging
+import time
 
 import tornado.web
 from tornado.escape import json_encode, json_decode
+from tornado.ioloop import IOLoop
 
 from utils.dotdict import DotDict
+from utils.misc import get_terminal_info_key
 from codes.errorcode import ErrorCode
 from constants import SMS
+from helpers.seqgenerator import SeqGenerator
+from helpers.gfsenderhelper import GFSenderHelper
+
 from mixin.base import  BaseMixin
 from base import BaseHandler, authenticated
        
@@ -30,6 +36,57 @@ class SwitchCarHandler(BaseHandler, BaseMixin):
                 self.bookkeep(dict(uid=self.current_user.uid,
                                    tid=tid,
                                    sim=terminal.sim))
+                def _on_finish(response):
+                    response = json_decode(response)
+                    if response['success'] == ErrorCode.SUCCESS:
+                        new_fobs = response['params']['FOBS']
+                        if new_fobs:
+                            fobs = self.db.query("SELECT fobid FROM T_FOB"
+                                                 "  WHERE tid = %s",
+                                                 tid)
+                            old_fobids = [fob.fobid for fob in fobs]
+                            new_fobids = new_fobs.split(':')
+                            add_fobids = list(set(new_fobids) - set(old_fobids))
+                            del_fobids = list(set(old_fobids) - set(new_fobids))
+                            for fobid in add_fobids:
+                                self.db.execute("INSERT INTO T_FOB(tid, fobid)"
+                                                "  VALUES(%s, %s)",
+                                                tid, fobid)
+                            for fobid in del_fobids:
+                                self.db.execute("DELETE FROM T_FOB"
+                                                "  WHERE tid = %s"
+                                                "    AND fobid = %s",
+                                                tid, fobid)
+                            if len(old_fobids) != len(new_fobids):
+                                self.db.execute("UPDATE T_TERMINAL_INFO"
+                                                "  SET keys_num = %s"
+                                                "  WHERE tid = %s",
+                                                len(new_fobids),
+                                                tid)
+                            # redis
+                            terminal_info_key = get_terminal_info_key(tid)
+                            terminal_info = self.redis.getvalue(terminal_info_key)
+                            if not terminal_info:
+                                terminal_info = DotDict(defend_status=None,
+                                                        mobile=None,
+                                                        login=None,
+                                                        gps=None,
+                                                        gsm=None,
+                                                        pbat=None,
+                                                        alias=None,
+                                                        keys_num=None,
+                                                        fob_list=[])
+                            terminal_info['fob_list'] = new_fobids
+                            self.redis.setvalue(terminal_info_key, terminal_info)
+                # send S5 for querying fobs 3 minutes later
+                args = DotDict(seq=SeqGenerator.next(self.db),
+                               tid=tid,
+                               params=['fobs',])
+                IOLoop.instance().add_timeout(int(time.time()) + 180,
+                                              lambda: GFSenderHelper.async_forward(
+                                                          GFSenderHelper.URLS.QUERY,
+                                                          args,
+                                                          _on_finish))
             else:
                 status = ErrorCode.LOGIN_AGAIN
             self.write_ret(status) 
