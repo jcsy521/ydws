32c15cf06bbaf15e636c621f6517da6b4c7b2bfc
jiaxiaolei <jiaxiaolei@fbfde4e3-4bc3-4d81-a63b-7b332075e0cf>
2012-10-30 03:12:40 +0000

    -- bug fixed: remove auth in checkupdata
    git-svn-id: http://www.ichebao.net/svn/acb/trunk@584 fbfde4e3-4bc3-4d81-a63b-7b332075e0cf

--- apps/uweb/handlers/checkupdate.py
+++ apps/uweb/handlers/checkupdate.py
@@ -12,21 +12,15 @@ from base import BaseHandler, authenticated
 
 class CheckUpdateHandler(BaseHandler):
 
-    @authenticated
     @tornado.web.removeslash
     def get(self):
         status = ErrorCode.SUCCESS
         try:
             version_info = None
-            terminal = QueryHelper.get_terminal_by_tid(self.current_user.tid, self.db)
-            if terminal:
-                version_info = get_version_info("android")
-            else:
-                status = ErrorCode.LOGIN_AGAIN
+            version_info = get_version_info("android")
             self.write_ret(status,
                            dict_=DotDict(version_info=version_info)) 
         except Exception as e:
-            logging.exception("[UWEB] Android check update failed. uid: %s, tid: %s. Exception: %s", 
-                              self.current_user.uid, self.current_user.tid, e.args) 
+            logging.exception("[UWEB] Android check update failed. Exception: %s", e.args) 
             status = ErrorCode.SERVER_BUSY
             self.write_ret(status)
