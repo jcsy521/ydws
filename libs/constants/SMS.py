879d6c279b17251a524e4fca0bf3690a278480b3
luojun <luojun@fbfde4e3-4bc3-4d81-a63b-7b332075e0cf>
2012-12-03 09:57:51 +0000

    LQ 30m
    git-svn-id: http://www.ichebao.net/svn/acb/trunk@631 fbfde4e3-4bc3-4d81-a63b-7b332075e0cf

--- libs/constants/SMS.py
+++ libs/constants/SMS.py
@@ -5,8 +5,8 @@ from utils.dotdict import DotDict
 
 SMS_EXPIRY=24*60*60
 
-LQ = DotDict(WEB=18*60, # 18h 
-             SMS=18*60) # 18h 
+LQ = DotDict(WEB=30, # 18h 
+             SMS=30) # 18h 
 
 LQ_INTERVAL = 3 * 60 # 3mins
 
