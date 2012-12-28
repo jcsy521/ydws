f3d8e4cac1a0bdc4b83009baa5c89889548539d7
luojun <luojun@fbfde4e3-4bc3-4d81-a63b-7b332075e0cf>
2012-12-28 09:14:57 +0000

    delete useless conf.
    git-svn-id: http://www.ichebao.net/svn/acb/trunk@672 fbfde4e3-4bc3-4d81-a63b-7b332075e0cf

--- conf.d/608_development/update_conf.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# swith login.html
-cd ./apps/uweb/templates/
-rm -f login.html
-ln -s 608_login.html login.html
-cd ../../../
-
-sed -i -e "s/--logging=warning/--mode=debug --logging=DEBUG/" ./conf/supervisord/services-available/*.conf
--- conf.d/ln_deployment/update_conf.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# swith login.html
-cd ./apps/uweb/templates/
-rm -f login.html
-ln -s ln_login.html login.html
-# cd ../../../
-# 
-# sed -i -e "s/--mode=debug//" -e "s/--logging=DEBUG/--logging=warning/" ./conf/supervisord/services-available/*.conf
