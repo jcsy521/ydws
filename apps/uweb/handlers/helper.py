27602b35e9b67120403d273dfecb138d32e30f85
kangjingjing <kangjingjing@fbfde4e3-4bc3-4d81-a63b-7b332075e0cf>
2013-01-21 06:19:18 +0000

    -- jump to the corresponding help page according to user_type
    git-svn-id: http://www.ichebao.net/svn/acb/trunk@765 fbfde4e3-4bc3-4d81-a63b-7b332075e0cf

--- apps/uweb/handlers/helper.py
+++ apps/uweb/handlers/helper.py
@@ -4,12 +4,17 @@ import logging
 
 import tornado.web
 
+from constants import UWEB
 from base import BaseHandler
 
 class HelperHandler(BaseHandler):
 
     @tornado.web.removeslash
     def get(self):
-        """Jump to help.html."""
-        self.render('helper.html')
+        """Jump to the coresponding help.html."""
+        user_type = self.get_argument('user_type',UWEB.USER_TYPE.PERSON)
+        if user_type == UWEB.USER_TYPE.PERSON:
+            self.render('helper.html')
+        else:
+            self.render('helper_corp.html')
 
