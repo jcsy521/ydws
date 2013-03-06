36cda6b617c6fe5ccf4e8eb491dc48e98cc9b96c
jiaxiaolei <jiaxiaolei@fbfde4e3-4bc3-4d81-a63b-7b332075e0cf>
2013-03-06 09:47:17 +0000

    -- add post method for logout/ios and logout/android
    git-svn-id: http://www.ichebao.net/svn/acb/trunk@875 fbfde4e3-4bc3-4d81-a63b-7b332075e0cf

--- apps/uweb/handlers/login.py
+++ apps/uweb/handlers/login.py
@@ -9,7 +9,7 @@ import tornado.web
 from tornado.escape import json_encode, json_decode
 
 from utils.dotdict import DotDict
-from utils.misc import get_ios_id_key, get_ios_badge_key
+from utils.misc import get_ios_id_key, get_ios_badge_key, get_terminal_info_key
 from utils.checker import check_sql_injection, check_phone
 from codes.errorcode import ErrorCode
 from constants import GATEWAY, UWEB
@@ -218,6 +218,46 @@ class IOSLogoutHandler(BaseHandler):
         self.redis.delete(*keys)
         self.clear_cookie(self.app_name)
 
+    @authenticated
+    @tornado.web.removeslash
+    def post(self):
+        """Clear the cookie and set defend."""
+        try:
+            data = DotDict(json_decode(self.request.body))
+        except:
+            self.write_ret(ErrorCode.ILLEGAL_DATA_FORMAT) 
+            logging.error("[UWEB] illegal format, body:%s", self.request.body)
+        else:
+            # 1: if there are tids, set defend
+            for tid in data.tids:
+                terminal = QueryHelper.get_terminal_by_tid(tid, self.db)
+                if not terminal:
+                    status = ErrorCode.LOGIN_AGAIN
+                    logging.error("The terminal with tid: %s does not exist, check it ", tid)
+                    continue
+
+                #self.keep_waking(self.current_user.sim, self.current_user.tid)
+                self.db.execute("UPDATE T_TERMINAL_INFO"
+                                "  SET mannual_status = %s"
+                                "  WHERE tid = %s",
+                                UWEB.DEFEND_STATUS.YES, tid)
+
+                terminal_info_key = get_terminal_info_key(tid)
+                terminal_info = self.redis.getvalue(terminal_info_key)
+                if terminal_info:
+                    terminal_info['mannual_status'] = UWEB.DEFEND_STATUS.YES
+                    self.redis.setvalue(terminal_info_key, terminal_info)
+                logging.info("[UWEB] uid:%s, tid:%s set mannual status  successfully", 
+                             self.current_user.uid, tid)
+        finally:
+            #2: clear cookie
+            ios_id_key = get_ios_id_key(self.current_user.uid)
+            ios_badge_key = get_ios_badge_key(self.current_user.uid)
+            keys = [ios_id_key, ios_badge_key]
+            self.redis.delete(*keys)
+            self.clear_cookie(self.app_name)
+
+
 class AndroidLogoutHandler(BaseHandler):
 
     @authenticated
@@ -226,3 +266,37 @@ class AndroidLogoutHandler(BaseHandler):
         """Clear the cookie ."""
         self.clear_cookie(self.app_name)
 
+    @authenticated
+    @tornado.web.removeslash
+    def post(self):
+        """Clear the cookie and set defend."""
+        try:
+            data = DotDict(json_decode(self.request.body))
+        except:
+            self.write_ret(ErrorCode.ILLEGAL_DATA_FORMAT) 
+            logging.error("[UWEB] illegal format, body:%s", self.request.body)
+        else:
+            # 1: if there are tids, set defend
+            for tid in data.tids:
+                terminal = QueryHelper.get_terminal_by_tid(tid, self.db)
+                if not terminal:
+                    status = ErrorCode.LOGIN_AGAIN
+                    logging.error("The terminal with tid: %s does not exist, check it ", tid)
+                    continue
+
+                #self.keep_waking(self.current_user.sim, self.current_user.tid)
+                self.db.execute("UPDATE T_TERMINAL_INFO"
+                                "  SET mannual_status = %s"
+                                "  WHERE tid = %s",
+                                UWEB.DEFEND_STATUS.YES, tid)
+
+                terminal_info_key = get_terminal_info_key(tid)
+                terminal_info = self.redis.getvalue(terminal_info_key)
+                if terminal_info:
+                    terminal_info['mannual_status'] = UWEB.DEFEND_STATUS.YES
+                    self.redis.setvalue(terminal_info_key, terminal_info)
+                logging.info("[UWEB] uid:%s, tid:%s set mannual status  successfully", 
+                             self.current_user.uid, tid)
+        finally:
+            #2: clear cookie
+            self.clear_cookie(self.app_name)
