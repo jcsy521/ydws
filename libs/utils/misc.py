96cd39e2f45049c59fdeab6299e636fd8de3fbce
kangjingjing <kangjingjing@fbfde4e3-4bc3-4d81-a63b-7b332075e0cf>
2013-03-13 01:47:40 +0000

    -- add misc.py to svnwq
    git-svn-id: http://www.ichebao.net/svn/acb/trunk@895 fbfde4e3-4bc3-4d81-a63b-7b332075e0cf

--- libs/utils/misc.py
+++ libs/utils/misc.py
@@ -63,6 +63,10 @@ def get_ios_badge_key(umobile):
     """for the ios badge of ios."""
     return str("ios_badge:%s" % umobile)
 
+def get_lastinfo_key(umobile):
+    """for the newest lastinfo of owner"""
+    return str("lastinfo:%s" % umobile)
+
 def get_location_key(dev_id):
     """for the newest location of dev"""
     return str("location:%s" % dev_id)
