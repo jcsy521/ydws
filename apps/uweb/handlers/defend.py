a52f22cfec5b9ee811e46fbd48cf25aedeed6318
kangjingjing <kangjingjing@fbfde4e3-4bc3-4d81-a63b-7b332075e0cf>
2013-03-20 06:21:28 +0000

    -- add tids in defend post
    git-svn-id: http://www.ichebao.net/svn/acb/trunk@940 fbfde4e3-4bc3-4d81-a63b-7b332075e0cf

--- apps/uweb/handlers/defend.py
+++ apps/uweb/handlers/defend.py
@@ -66,42 +66,42 @@ class DefendHandler(BaseHandler, BaseMixin):
         status = ErrorCode.SUCCESS
         try:
             data = DotDict(json_decode(self.request.body))
-            tid = data.get('tid',None) 
+            tid = data.get('tid', None) 
+            tids = data.get('tids', None)
             # check tid whether exist in request and update current_user
             self.check_tid(tid)
-            logging.info("[UWEB] defend request: %s, uid: %s, tid: %s", 
-                         data, self.current_user.uid, self.current_user.tid)
+            logging.info("[UWEB] defend request: %s, uid: %s, tid: %s, tids: %s", 
+                         data, self.current_user.uid, self.current_user.tid, tids)
         except Exception as e:
             status = ErrorCode.ILLEGAL_DATA_FORMAT
             self.write_ret(status)
             return 
 
         try:
-            terminal = self.db.get("SELECT id FROM T_TERMINAL_INFO"
-                                   "  WHERE tid = %s"
-                                   "    AND service_status = %s",
-                                   self.current_user.tid,
-                                   UWEB.SERVICE_STATUS.ON)
-            if not terminal:
-                status = ErrorCode.LOGIN_AGAIN
-                logging.error("The terminal with tid: %s does not exist, redirect to login.html", self.current_user.tid)
-                self.write_ret(status)
-                return
+            for tid in tids:
+                terminal = self.db.get("SELECT id FROM T_TERMINAL_INFO"
+                                       "  WHERE tid = %s"
+                                       "    AND service_status = %s",
+                                       tid, UWEB.SERVICE_STATUS.ON)
+                if not terminal:
+                    status = ErrorCode.LOGIN_AGAIN
+                    logging.error("The terminal with tid: %s does not exist, redirect to login.html", tid)
+                    self.write_ret(status)
+                    return
 
-            self.keep_waking(self.current_user.sim, self.current_user.tid)
-            self.db.execute("UPDATE T_TERMINAL_INFO"
-                            "  SET mannual_status = %s"
-                            "  WHERE tid = %s",
-                            data.mannual_status, self.current_user.tid)
+                #self.keep_waking(self.current_user.sim, self.current_user.tid)
+                self.db.execute("UPDATE T_TERMINAL_INFO"
+                                "  SET mannual_status = %s"
+                                "  WHERE tid = %s",
+                                data.mannual_status, tid)
 
-            terminal_info_key = get_terminal_info_key(self.current_user.tid)
-            terminal_info = self.redis.getvalue(terminal_info_key)
-            if terminal_info:
-                terminal_info['mannual_status'] = data.mannual_status
-                self.redis.setvalue(terminal_info_key, terminal_info)
-            logging.info("[UWEB] uid:%s, tid:%s set mannual status to %s successfully", 
-                         self.current_user.uid, self.current_user.tid,
-                         data.mannual_status)
+                terminal_info_key = get_terminal_info_key(tid)
+                terminal_info = self.redis.getvalue(terminal_info_key)
+                if terminal_info:
+                    terminal_info['mannual_status'] = data.mannual_status
+                    self.redis.setvalue(terminal_info_key, terminal_info)
+                logging.info("[UWEB] uid:%s, tid:%s set mannual status to %s successfully", 
+                             self.current_user.uid, tid, data.mannual_status)
             self.write_ret(status)
         except Exception as e:
             logging.exception("[UWEB] uid:%s, tid:%s set mannual status to %s failed. Exception: %s", 
