e1d4dc71d07998b8015e3d13ae53a2a78ea2d991
jiaxiaolei <jiaxiaolei@fbfde4e3-4bc3-4d81-a63b-7b332075e0cf>
2013-05-23 01:33:45 +0000

    add ',' filter in check sql inject
    git-svn-id: http://www.ichebao.net/svn/acb/trunk@1131 fbfde4e3-4bc3-4d81-a63b-7b332075e0cf

--- libs/utils/checker.py
+++ libs/utils/checker.py
@@ -7,7 +7,7 @@ _sql_injection_pattern ='|'.join((r"\b(select|update|insert|delete|drop|create|a
                                   r"\b(execute|prepare|call|start|lock|change|use)\b",
                                   r"\b(where|from|join|like)\b",
                                   r"--|#",
-                                  r";|&|\\|\*|\^|\(|\)|\%|\$|\>|\<"))
+                                  r";|&|\'|\\|\*|\^|\(|\)|\%|\$|\>|\<"))
 
 SQL_INJECTION_CHECKER = re.compile(_sql_injection_pattern, re.I)
 
