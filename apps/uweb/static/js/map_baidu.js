a4f28ec71fa73876c515953617fd4d6238ffda4a
guozhaoxia <guozhaoxia@fbfde4e3-4bc3-4d81-a63b-7b332075e0cf>
2013-06-13 08:06:50 +0000

    --hs: 解决变量找不到问题
    git-svn-id: http://www.ichebao.net/svn/acb/trunk@1208 fbfde4e3-4bc3-4d81-a63b-7b332075e0cf

--- apps/uweb/static/js/map_baidu.js
+++ apps/uweb/static/js/map_baidu.js
@@ -375,8 +375,8 @@ window.dlf.fn_tipContents = function (obj_location, str_iconType, n_index) {
 					str_oldClon = 0,
 					str_oldClat = 0,
 					str_newClon = obj_location.clongitude,
-					str_newClat = obj_location.clatitude;
-					//obj_carsData = $('.j_carList').data('carsData');
+					str_newClat = obj_location.clatitude,
+					obj_carsData = $('.j_carList').data('carsData');
 				
 				if ( obj_carsData != undefined ) {
 					obj_oldCarData = obj_carsData[str_tid];
