8b6f9f9a89773bb02501ba902c873346a1d1815b
hubo <hubo@fbfde4e3-4bc3-4d81-a63b-7b332075e0cf>
2013-07-25 03:54:05 +0000

    --LOG系统中短信解绑bug修改，功能：JB短信下发后设置终端为待解绑
    git-svn-id: http://www.ichebao.net/svn/acb/trunk@1329 fbfde4e3-4bc3-4d81-a63b-7b332075e0cf

--- apps/log/handlers/message.py
+++ apps/log/handlers/message.py
@@ -22,7 +22,7 @@ class MessageHandler(BaseHandler):
     def get(self):
         username = self.get_current_user()
         n_role = self.db.get("select role from T_LOG_ADMIN where name = %s", username)
-	domain = ConfHelper.GW_SERVER_CONF.domain
+        domain = ConfHelper.GW_SERVER_CONF.domain
         domain_ip = ConfHelper.GW_SERVER_CONF.domain_ip
         domains = [domain,domain_ip]
         self.render('sms/sms.html',
@@ -38,13 +38,13 @@ class MessageHandler(BaseHandler):
             data = json_decode(self.request.body)
             sms_type = data.get('sms_type')
             tmobile = data.get('tmobile')
+            umobile = data.get('umobile')
             content = ''
             if check_phone(tmobile) is None:
                 status = ErrorCode.ILLEGAL_MOBILE
                 self.write_ret(status) 
             else:
                 if sms_type == 'JH':
-                    umobile = data.get('umobile')
                     if check_phone(umobile):
                         content = ':SIM' + ' ' + umobile + ':' + tmobile
                         SMSHelper.send_to_terminal(tmobile,content)
@@ -55,7 +55,22 @@ class MessageHandler(BaseHandler):
                 elif sms_type == 'JB':
                     content = ':' + sms_type
                     is_clear = data.get('is_clear')
-                    SMSHelper.send_to_terminal(tmobile,content)
+                    ret = SMSHelper.send_to_terminal(tmobile,content)
+                    ret = json_decode(ret)
+                    if ret['status'] == 0:
+                        self.acbdb.execute("UPDATE T_TERMINAL_INFO"
+                                        "  SET service_status = 2"
+                                        "  WHERE mobile = %s",
+                                        tmobile)
+                        terminals = self.acbdb.query("SELECT id FROM T_TERMINAL_INFO"
+                                                  "  WHERE owner_mobile = %s"
+                                                  "    AND service_status = 1",
+                                                  umobile)
+                        # clear user
+                        if len(terminals) == 0:
+                            self.acbdb.execute("DELETE FROM T_USER"
+                                            "  WHERE mobile = %s",
+                                            umobile)
                     if is_clear == 1:
                         clear_data(tmobile,self.acbdb)
                     self.write_ret(status)
