b89c6cb754a694769c053104825996ccb02a59c1
jiaxiaolei <jiaxiaolei@fbfde4e3-4bc3-4d81-a63b-7b332075e0cf>
2013-08-18 15:57:45 +0000

    --Jia Xiaolei -- use delete_terminal method in utils.public
    git-svn-id: http://www.ichebao.net/svn/acb/trunk@1380 fbfde4e3-4bc3-4d81-a63b-7b332075e0cf

--- misc/delete_terminal.py
+++ misc/delete_terminal.py
@@ -16,48 +16,28 @@ from helpers.smshelper import SMSHelper
 from helpers.queryhelper import QueryHelper
 from db_.mysql import DBConnection
 from utils.myredis import MyRedis
+from utils.public import delete_terminal
 from utils.misc import *
 
-def delete_terminal(tid, db, redis):
-    user = QueryHelper.get_user_by_tid(tid, db)
-    terminal = db.get("SELECT mobile FROM T_TERMINAL_INFO"
-                      "  WHERE tid = %s", tid)
-    for item in [tid,]:
-        sessionID_key = get_terminal_sessionID_key(item)
-        address_key = get_terminal_address_key(item)
-        info_key = get_terminal_info_key(item)
-        lq_sms_key = get_lq_sms_key(item)
-        lq_interval_key = get_lq_interval_key(item)
-        location_key = get_location_key(item)
-        del_data_key = get_del_data_key(item)
-        keys = [sessionID_key, address_key, info_key, lq_sms_key, lq_interval_key,
-                location_key, del_data_key]
-        redis.delete(*keys)
-    # clear db
-    db.execute("DELETE FROM T_TERMINAL_INFO"
-               "  WHERE tid = %s", 
-               tid) 
-    logging.info("[GW] Delete Terminal: %s, umobile: %s",
-                 tid, (user.owner_mobile if user else None))
 
-def execute(tid):
+def execute(mobile):
     db = DBConnection().db
     redis = MyRedis()
-    delete_terminal(tid, db, redis)
-    
-
+    terminal = db.get("SELECT tid, mobile FROM T_TERMINAL_INFO WHERE mobile = %s LIMIT 1", mobile)
+    if terminal:
+        delete_terminal(terminal.tid, db, redis, del_user=True)
 
 def main():
     ConfHelper.load('../conf/global.conf')
     parse_command_line()
-    if not options.tid:
+    if not options.mobile:
         usage()
         exit(1)
-    execute(options.tid)
+    execute(options.mobile)
     
 
 def usage():
-    print "Usage: python delete_terminal.py --tid=1111"
+    print "Usage: python delete_terminal.py --mobile=15919176710"
 
 if __name__ == "__main__": 
     main()
