d9b65eba00ee97aa1e948fe52a982674ecf23bfc
zhangzhenlong <zhangzhenlong@fbfde4e3-4bc3-4d81-a63b-7b332075e0cf>
2013-08-23 02:00:56 +0000

    执法局数据接口
    git-svn-id: http://www.ichebao.net/svn/acb/trunk@1393 fbfde4e3-4bc3-4d81-a63b-7b332075e0cf

--- apps/uweb/handlers/zfjsyncer.py
+++ apps/uweb/handlers/zfjsyncer.py
@@ -2,6 +2,7 @@
 
 import logging
 import time
+import re
 
 import tornado.web
 
@@ -12,25 +13,29 @@ from base import BaseHandler, authenticated
 
 class ZFJSyncerHandler(BaseHandler):
 
-    @authenticated
     @tornado.web.removeslash
     def post(self):
+        res = list()
         try:
-            res = list()
             corp_id = 13928191116   #东升执法局
-            begin_time = self.redis.getvalue('last_time')
-            end_time = time.time()
-            if begin_time:
-                terminals = QueryHelper.get_terminals_by_cid(corp_id,self.db)
-                for terminal in terminals:
-                    mobile = terminal['mobile']
-                    tid = terminal['tid']
-                    positions = self.db.query("SELECT latitude, longitude, timestamp FROM T_LOCATION WHERE tid=%s AND timestamp BETWEEN %s AND %s ORDER BY timestamp", tid, begin_time , end_time)
-                    res.append({'mobile':mobile, 'positions':positions})
-            self.redis.setvalue('last_time', end_time)
-            self.write({'res':res})
-
+            ip = self.request.remote_ip
+            ips = re.compile('192.168.1.*')
+            if ips.match(ip):
+                begin_time = self.redis.getvalue('last_time')
+                end_time = time.time()
+                if begin_time:
+                    terminals = QueryHelper.get_terminals_by_cid(corp_id,self.db)
+                    for terminal in terminals:
+                        mobile = terminal['mobile']
+                        tid = terminal['tid']
+                        positions = self.db.query("SELECT latitude, longitude, timestamp FROM T_LOCATION WHERE tid=%s AND timestamp BETWEEN %s AND %s ORDER BY timestamp", tid, begin_time , end_time)
+                        res.append({'mobile':mobile, 'positions':positions})
+                self.redis.setvalue('last_time', end_time)
+                self.write({'res':res})
+            else:
+                self.write({'res':res})
+                logging.error("Error: wrong ip.")
         except Exception as e:
-            self.render('errors/error.html',message=ErrorCode.ERROR_MESSAGE[ErrorCode.FAILED])
+            self.write({'res':res})
             logging.exception("[UWEB] zfjsyncer: get location failed. Exception: %s", e.args) 
 
