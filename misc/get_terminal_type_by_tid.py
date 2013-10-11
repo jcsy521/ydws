0c0a70d662a8befb246916669f7d7db45154f19c
jiaxiaolei <jiaxiaolei@fbfde4e3-4bc3-4d81-a63b-7b332075e0cf>
2013-10-11 07:22:51 +0000

    get terminal type (ZJ100 or ZJ200) by tid(SN)
    git-svn-id: http://www.ichebao.net/svn/acb/trunk@1472 fbfde4e3-4bc3-4d81-a63b-7b332075e0cf

---
+++ misc/get_terminal_type_by_tid.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+
+import sys
+import os.path
+from tornado.options import define, options, parse_command_line
+
+define('tid', default="")
+
+# global definition
+# base = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, A, B, C, D, E, F]
+base = [str(x) for x in range(10)] + [ chr(x) for x in range(ord('A'),ord('A')+6)]
+
+# hex2dec
+# 十六进制 to 十进制
+def hex2dec(string_num):
+    return str(int(string_num.upper(), 16))
+
+# dec2bin
+# 十进制 to 二进制: bin() 
+def dec2bin(string_num):
+    num = int(string_num)
+    mid = []
+    while True:
+        if num == 0: break
+        num,rem = divmod(num, 2)
+        mid.append(base[rem])
+
+    return ''.join([str(x) for x in mid[::-1]])
+
+# hex2tobin
+# 十六进制 to 二进制: bin(int(str,16)) 
+def hex2bin(string_num):
+    return dec2bin(hex2dec(string_num.upper()))
+
+def usage():
+    print "Usage: python get_terminal_type_by_tid.py --tid=[tid]"
+
+def main():
+    parse_command_line()
+    if not 'tid' in options:
+        usage()
+        exit(1)
+
+    tid = options.tid
+    
+    bin_tid = hex2bin(tid)
+    print 'bin tid:', bin_tid
+    ttype = bin_tid[15:18]
+    if ttype == "000":
+        print 'ZJ100'
+    elif ttype == '100':
+        print 'ZJ200'
+    else:
+        print 'Unknow terminal type'
+
+if __name__ == "__main__":
+    main()
