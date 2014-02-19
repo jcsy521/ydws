3b0bb8bd15cedb8ef85fa928641d775c220179b7
jiaxiaolei <jiaxiaolei@fbfde4e3-4bc3-4d81-a63b-7b332075e0cf>
2014-02-19 03:33:46 +0000

    -- Jia Xiaolei -- bug fixed
    git-svn-id: http://www.ichebao.net/svn/acb/trunk@1646 fbfde4e3-4bc3-4d81-a63b-7b332075e0cf

--- apps/sms.d/sms_zs/util/des.py
+++ apps/sms.d/sms_zs/util/des.py
@@ -11,21 +11,16 @@ from tornado.options import define, options
 if 'conf' not in options:
     define('conf', default=os.path.join(TOP_DIR_, "conf/global.conf"))
 from helpers.confhelper import ConfHelper
+from utils.misc import safe_utf8
 
 class DES(object):
     
     def __init__(self, data):
-#        print " data type : ", type(data)
         ConfHelper.load(options.conf)
-        data = data.encode('utf-8')
+        data = safe_utf8(data)
         length = str(len(data))
-        bytes = struct.pack(length + 's',data)
+        bytes = struct.pack(length + 's', data)
         des_key = ConfHelper.SMS_CONF.des_key
         k = des(des_key, CBC, des_key, pad=None, padmode=PAD_PKCS5)
         self.result = k.encrypt(bytes)
-
-#        print "Encrypted: %r" % self.result
-#        print "Decrypted: %r" % k.decrypt(self.result)
-#        assert k.decrypt(self.result, padmode=PAD_PKCS5) == data
-        
         
--- apps/sms.d/sms_zs/util/md5.py
+++ apps/sms.d/sms_zs/util/md5.py
@@ -2,12 +2,13 @@
 # -*- coding: utf-8 -*-
 
 import hashlib
+from utils.misc import safe_utf8
 
 class MD5(object):
     
     def __init__(self, str):
         hash_object = hashlib.md5()
-        hash_object.update(str.encode('utf-8'))
+        hash_object.update(safe_utf8(str))
         self.result = hash_object.hexdigest()
         
         
