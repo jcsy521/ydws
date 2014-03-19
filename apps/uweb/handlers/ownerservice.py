b275c65b34aa89de73385408e72c7527a40392c0
sunyoubo <sunyoubo@fbfde4e3-4bc3-4d81-a63b-7b332075e0cf>
2014-03-19 03:56:32 +0000

    git-svn-id: http://www.ichebao.net/svn/acb/trunk@1707 fbfde4e3-4bc3-4d81-a63b-7b332075e0cf

--- apps/uweb/handlers/ownerservice.py
+++ apps/uweb/handlers/ownerservice.py
@@ -9,13 +9,11 @@ from tornado.escape import json_decode
 
 class OwnerserviceHandler(BaseHandler):
 
-    @authenticated
     @tornado.web.removeslash
     def get(self):
         username = self.get_current_user()
         self.render("html/ownerservice.html", username=username)
 
-    @authenticated
     @tornado.web.removeslash
     def post(self):
         status = ErrorCode.SUCCESS
@@ -31,8 +29,6 @@ class OwnerserviceHandler(BaseHandler):
                     self.db.execute("INSERT INTO T_OWNERSERVICE(umobile, cnum, add_time, car_type)"
                                     "VALUES(%s,%s,%s,%s)", umobile, cnum, add_time, car_type
                                     )
-                else:
-                    print("you need add umobile and car number")
 
             self.write_ret(status=status)
             # if status= 201?
