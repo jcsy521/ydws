12f121a33ae625ebf1a12ae0072a5c66b66d12b6
sunyoubo <sunyoubo@fbfde4e3-4bc3-4d81-a63b-7b332075e0cf>
2014-03-20 10:06:43 +0000

    --hs --Admin,部分查询导入时的逻辑判断
    git-svn-id: http://www.ichebao.net/svn/acb/trunk@1728 fbfde4e3-4bc3-4d81-a63b-7b332075e0cf

--- apps/admin/static/js/dataTable.js
+++ apps/admin/static/js/dataTable.js
@@ -1,8 +1,6 @@
 ﻿var oTable,
 	rTable,
-	sTable,
-	binglogTable,
-	tlocationTable;
+	sTable;
 $(function () {
 	oTable = $('#rDataTables').dataTable({
 		"bScrollCollapse": true,
@@ -58,48 +56,6 @@ $(function () {
 		}
 	});
 	$('#staticsTables tr').hover(function(){
-		$(this).children().css({
-			'background-color' : '#87CEFF'
-		});
-	},
-	function(){
-		$(this).children().removeAttr('style');
-	});
-	// 终端绑定记录查询
-	binglogTable = $('#tbindlogDataTables').dataTable({
-		"bScrollCollapse": true,
-		'aaSorting':[], // 默认不排序
-		"bJQueryUI": true,
-		"bProcessing": true,
-		"sPaginationType": "full_numbers",  
-		"aLengthMenu": [10, 20, 50, 100], //每页显示可调
-		"iDisplayLength":20,//默认每页10条记录
-		"oLanguage": {
-			"sUrl":"/static/js/dataTables.zh_CN.txt"
-		}
-	});
-	$('#tbindlogDataTables tr').hover(function(){
-		$(this).children().css({
-			'background-color' : '#87CEFF'
-		});
-	},
-	function(){
-		$(this).children().removeAttr('style');
-	});
-	// 终端位置查询
-	tlocationTable = $('#tlocationDataTables').dataTable({
-		"bScrollCollapse": true,
-		'aaSorting':[], // 默认不排序
-		"bJQueryUI": true,
-		"bProcessing": true,
-		"sPaginationType": "full_numbers",  
-		"aLengthMenu": [10, 20, 50, 100], //每页显示可调
-		"iDisplayLength":20,//默认每页10条记录
-		"oLanguage": {
-			"sUrl":"/static/js/dataTables.zh_CN.txt"
-		}
-	});
-	$('#tlocationDataTables tr').hover(function(){
 		$(this).children().css({
 			'background-color' : '#87CEFF'
 		});
