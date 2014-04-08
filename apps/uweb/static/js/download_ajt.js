808a782926a444497277297d21d3d256b49db18d
jiaxiaolei <jiaxiaolei@fbfde4e3-4bc3-4d81-a63b-7b332075e0cf>
2014-04-08 10:11:26 +0000

    --hs --隐藏aj的IOS下载
    git-svn-id: http://www.ichebao.net/svn/acb/trunk@1809 fbfde4e3-4bc3-4d81-a63b-7b332075e0cf

--- apps/uweb/static/js/download_ajt.js
+++ apps/uweb/static/js/download_ajt.js
@@ -4,6 +4,8 @@
 $(function() { 
 	var obj_userAgent = navigator.userAgent;
 	
+	$('.androidContent').show();
+	return;
 	if ( obj_userAgent.match('Android') || obj_userAgent.match('Linux') ) {
 		$('.androidContent').show();
 	} else {
