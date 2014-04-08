ed20b36139ac9c3de17d4d9c5253f5750032d3bc
jiaxiaolei <jiaxiaolei@fbfde4e3-4bc3-4d81-a63b-7b332075e0cf>
2014-04-08 06:39:43 +0000

    --hs --ajt logo update
    git-svn-id: http://www.ichebao.net/svn/acb/trunk@1806 fbfde4e3-4bc3-4d81-a63b-7b332075e0cf

--- apps/uweb/static/js/judge_logo.js
+++ apps/uweb/static/js/judge_logo.js
@@ -3,9 +3,6 @@
 */
 var str_logUrl = '';
 
-if ( DOMAIN_HOST == 'ajt.ydcws.com' || DOMAIN_HOST == 'ajt.ichebao.net' ) { 
-	str_logUrl = 'bannerAd_ajt.png';
-} else {
-	str_logUrl = 'bannerAd.png';
-}
-$('#ads').css('background-image', 'url(/static/images/'+ str_logUrl +')');
+if ( DOMAIN_HOST == 'ajt.zhydgps.com' || DOMAIN_HOST == 'ajt.ichebao.net' ) { 
+	$('#ads').css('background-image', 'url("/static/images/bannerAd_ajt.png")');
+} 
