1ecc312b4320d1dfd709e02cebbbb381cde1718a
jiaxiaolei <jiaxiaolei@fbfde4e3-4bc3-4d81-a63b-7b332075e0cf>
2014-04-19 03:34:30 +0000

    --Jia Xiaolei --use multi-offset
    git-svn-id: http://www.ichebao.net/svn/acb/trunk@1825 fbfde4e3-4bc3-4d81-a63b-7b332075e0cf

--- apps/mycelery/dbtask.py
+++ apps/mycelery/dbtask.py
@@ -17,7 +17,7 @@ from db_.mysql import get_connection
 from helpers.confhelper import ConfHelper
 from utils.dotdict import DotDict
 from utils.misc import DUMMY_IDS
-from helpers.lbmphelper import get_clocation_from_ge 
+from helpers.lbmphelper import get_clocation_from_ge, get_locations_with_clatlon
 
 if not 'conf' in options:
     define('conf', default=os.path.join(TOP_DIR_, "conf/global.conf"))
@@ -38,22 +38,24 @@ class DBTask(object):
         # get the points without invalid clatlon within 5 minutes, and try to provide
         # valid clatlon for them
         logging.info("[CLERY] dbtask update clatclon started.")
-        points = self.db.query("SELECT id, latitude, longitude, clatitude, clongitude"
+        points = self.db.query("SELECT id, latitude, longitude, clatitude, clongitude, type, timestamp"
                                "  FROM T_LOCATION"
                                "  WHERE (clatitude = 0 or clongitude =0) "
                                "  AND latitude != 0 AND longitude !=0 "
                                "  AND (timestamp between %s and %s)",
-                               int(time.time())-60*5, int(time.time()))
+                               int(time.time())-60*2, int(time.time()))
 
         logging.info("[CLERY] the number of points without valid claton: %s.", len(points))
-        for point in points:
-            time.sleep(1)
-            clats, clons = get_clocation_from_ge([point['latitude'],], [point['longitude'],]) 
-            clat, clon = clats[0], clons[0]
-            self.db.execute("UPDATE T_LOCATION"
-                            "  SET clatitude = %s, clongitude = %s"
-                            "  WHERE id = %s",
-                            clat, clon, point['id'])
+
+        get_locations_with_clatlon(points, self.db)
+        #for point in points:
+        #    time.sleep(0.1)
+        #    clats, clons = get_clocation_from_ge([point['latitude'],], [point['longitude'],]) 
+        #    clat, clon = clats[0], clons[0]
+        #    self.db.execute("UPDATE T_LOCATION"
+        #                    "  SET clatitude = %s, clongitude = %s"
+        #                    "  WHERE id = %s",
+        #                    clat, clon, point['id'])
 
 def update_clatclon():
     dbt = DBTask()
