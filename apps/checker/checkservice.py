422130bd713457d906685756d22390d2bd8d89cb
jiaxiaolei <jiaxiaolei@fbfde4e3-4bc3-4d81-a63b-7b332075e0cf>
2014-04-23 16:25:20 +0000

    --Jia Xiaolei --1: use jiaxiaolei's mobile
    git-svn-id: http://www.ichebao.net/svn/acb/trunk@1858 fbfde4e3-4bc3-4d81-a63b-7b332075e0cf

--- apps/checker/checkservice.py
+++ apps/checker/checkservice.py
@@ -20,7 +20,7 @@ class CheckService(object):
     def __init__(self):
         self.db = DBConnection().db
         self.tid = 'B123SIMULATOR'
-        self.mobiles = [13693675352, 15901258591,13581731204]
+        self.mobiles = [13693675352, 18310505991, 13581731204]
         self.emails = ['boliang.guan@dbjtech.com', 'youbo.sun@dbjtech.com', 'xiaolei.jia@dbjtech.com']
         #self.emails = ['boliang.guan@dbjtech.com', 'zhaoxia.guo@dbjtech.com', 'xiaolei.jia@dbjtech.com', 'yuanchanggang@sxt.com.cn']
         
