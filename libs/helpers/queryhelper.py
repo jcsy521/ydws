7aad41ca5f7233b30063c47244f0370d494fa58e
jiaxiaolei <jiaxiaolei@fbfde4e3-4bc3-4d81-a63b-7b332075e0cf>
2014-04-23 01:49:48 +0000

    --Jia Xiaolei --remove query d
    git-svn-id: http://www.ichebao.net/svn/acb/trunk@1851 fbfde4e3-4bc3-4d81-a63b-7b332075e0cf

--- libs/helpers/queryhelper.py
+++ libs/helpers/queryhelper.py
@@ -247,15 +247,15 @@ class QueryHelper(object):
         location_key = get_location_key(str(tid))
         location = redis.getvalue(location_key)
         if not location:
-            location = db.get("SELECT id, speed, timestamp, category, name,"
-                              "  degree, type, latitude, longitude, clatitude, clongitude,"
-                              "  timestamp, locate_error"
-                              "  FROM T_LOCATION"
-                              "  WHERE tid = %s"
-                              "    AND NOT (latitude = 0 AND longitude = 0)"
-                              "    ORDER BY timestamp DESC"
-                              "    LIMIT 1",
-                              tid)
+            #location = db.get("SELECT id, speed, timestamp, category, name,"
+            #                  "  degree, type, latitude, longitude, clatitude, clongitude,"
+            #                  "  timestamp, locate_error"
+            #                  "  FROM T_LOCATION"
+            #                  "  WHERE tid = %s"
+            #                  "    AND NOT (latitude = 0 AND longitude = 0)"
+            #                  "    ORDER BY timestamp DESC"
+            #                  "    LIMIT 1",
+            #                  tid)
             if location:
                 mem_location = DotDict({'id':location.id,
                                         'latitude':location.latitude,
