cf05f303c7af7d68eb6169fa9b5af5e1eed25a4a
jiaxiaolei <jiaxiaolei@fbfde4e3-4bc3-4d81-a63b-7b332075e0cf>
2014-04-24 03:16:19 +0000

    --Jia Xiaolei --add ajt downlod
    git-svn-id: http://www.ichebao.net/svn/acb/branches/ajt@1859 fbfde4e3-4bc3-4d81-a63b-7b332075e0cf

--- apps/uweb/static/js/download_ajt.js
+++ apps/uweb/static/js/download_ajt.js
@@ -5,10 +5,12 @@ $(function() {
 	var obj_userAgent = navigator.userAgent;
 	
 	$('.androidContent').show();
-	return;
+	
 	if ( obj_userAgent.match('Android') || obj_userAgent.match('Linux') ) {
 		$('.androidContent').show();
+		$('.iosContent').hide();
 	} else {
+		$('.androidContent').hide();
 		$('.iosContent').show();
 	}
 });
