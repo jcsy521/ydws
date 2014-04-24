6a97a318b19d04fa9640a2e16f93c2e757244620
jiaxiaolei <jiaxiaolei@fbfde4e3-4bc3-4d81-a63b-7b332075e0cf>
2014-04-24 10:40:28 +0000

    --Jia Xiaolei --add gv in lbmp_test
    git-svn-id: http://www.ichebao.net/svn/acb/trunk@1861 fbfde4e3-4bc3-4d81-a63b-7b332075e0cf

--- misc/lbmp_test.sh
+++ misc/lbmp_test.sh
@@ -2,7 +2,10 @@
 #!/bin/bash
 
 # subscription
-#curl -d '{"sim":"14778471517", "action":"A"}' http://app01:3001/subscription
+curl -d '{"sim":"13011292221", "action":"A"}' http://app01:3001/subscription
 
 # le
-curl -d '{"sim":"14778477120","lac":"","cid":"","mcc":"","mnc":""}' http://app01:3001/le
+#curl -d '{"sim":"14778477120","lac":"","cid":"","mcc":"","mnc":""}' http://app01:3001/le
+
+# ge
+#curl -i -X POST  -d '{"lons":[408777840],"lats":[79824672]}' localhost:3000/ge
