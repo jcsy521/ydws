2d17cf1648873826b545212ab19529239c7e6587
jiaxiaolei <jiaxiaolei@fbfde4e3-4bc3-4d81-a63b-7b332075e0cf>
2014-06-27 03:36:18 +0000

    --Jia Xiaolei --add utils.sh
    git-svn-id: http://www.ichebao.net/svn/acb/trunk@2008 fbfde4e3-4bc3-4d81-a63b-7b332075e0cf

---
+++ misc/utils.sh
@@ -0,0 +1,15 @@
+
+#####################################################################
+# Here, just record some useful scrip to make work easier.          #
+#                                                                   #
+#                                                                   #
+#                                                                   #
+#                                                                   #
+#                                                                   #
+#                                                                   #
+#                                                                   #
+#####################################################################
+
+
+#NOTE: copy diff to localhost. to patch 
+scp w_jiaxiaolei@drone-009:/tmp/jia.diff .
