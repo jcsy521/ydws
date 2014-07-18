1012eccee2f85f9ed7b92768b81999547a126a34
jiaxiaolei <jiaxiaolei@fbfde4e3-4bc3-4d81-a63b-7b332075e0cf>
2014-07-18 10:36:14 +0000

    --Jia Xiaolei add key
    git-svn-id: http://www.ichebao.net/svn/acb/trunk@2041 fbfde4e3-4bc3-4d81-a63b-7b332075e0cf

--- libs/utils/misc.py
+++ libs/utils/misc.py
@@ -195,6 +195,10 @@ def get_group_terminal_detail_key(cid, gid, tid):
     """for the terminal detail belongs to cid, gid"""
     return str("terminaldetail:%s:%s:%s" % (cid, gid, tid))
 
+def get_acc_status_info_key(tid):
+    """for the acc_status info belongs to the tid"""
+    return str("acc_status_info:%s" % tid)
+
 def get_terminal_time(timestamp):
     """Format a readable time like 2013-10-10，10:10:10 
     """
