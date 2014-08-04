3dc04d1577eaf46ca43d0eacd467d1d09302d93b
pabb <pabb@fbfde4e3-4bc3-4d81-a63b-7b332075e0cf>
2014-08-04 06:23:57 +0000

    --Jia Xiaolei -- use python interpreter
    git-svn-id: http://www.ichebao.net/svn/acb/trunk@2093 fbfde4e3-4bc3-4d81-a63b-7b332075e0cf

--- apps/admin/start.sh
+++ apps/admin/start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 me=$(dirname $0)
-python2.6 -tt $me/server.py $1 $2 --mode=debug --logging=DEBUG 
+python -tt $me/server.py $1 $2 --mode=debug --logging=DEBUG 
 #--conf=../../conf/global.conf --port=8000
