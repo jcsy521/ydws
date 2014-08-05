7989caef77794fc646cc23500ecb2ab73c852a84
pabb <pabb@fbfde4e3-4bc3-4d81-a63b-7b332075e0cf>
2014-08-05 05:47:27 +0000

    --Jia Xiaolei --rich logs
    git-svn-id: http://www.ichebao.net/svn/acb/trunk@2107 fbfde4e3-4bc3-4d81-a63b-7b332075e0cf

--- apps/sms.d/sms_bj/handlers/acbmthandler.py
+++ apps/sms.d/sms_bj/handlers/acbmthandler.py
@@ -19,18 +19,22 @@ class ACBMTHandler(BaseHandler):
         msgid = str(insert_time)[-9:]
         try:
             content = self.get_argument("content")
-            logging.info("content = %s", content)
             mobile = self.get_argument("mobile")
+            logging.info("[SMS] mobile = %s,  content = %s", mobile, content)
+            if mobile.startswith('20000'): # test trackers
+                logging.info("[SMS] %s starts with 20000, so skip the sms.", mobile)
+                self.write({'status' : ErrorCode.SUCCESS, 'msgid' : msgid})
+                return
             
             self.db.execute("INSERT INTO T_SMS(msgid, mobile, content, "
                             " insert_time, category, send_status) "
                             "  VALUES(%s, %s, %s, %s, %s, %s)",
                             msgid, mobile, content, insert_time,
                             SMS.CATEGORY.MT, SMS.SENDSTATUS.PREPARING)
-            logging.info("acb-->sms save success! mobile = %s, content = %s", mobile, content)
+            logging.info("[SMS] acb-->sms save success! mobile = %s, content = %s", mobile, content)
             self.write({'status' : ErrorCode.SUCCESS, 'msgid' : msgid})
         except Exception, msg:  
-            logging.exception("acb-->sms post exception : %s", msg)
+            logging.exception("[SMS] acb-->sms post exception : %s", msg)
             self.write({'status' : ErrorCode.FAILED, 'msgid' : msgid})
         
         
