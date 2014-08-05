0ac23214102ce413a7770d432c5d705455cab02e
jiaxiaolei <jiaxiaolei@fbfde4e3-4bc3-4d81-a63b-7b332075e0cf>
2014-08-05 09:24:45 +0000

    --Jia Xiaolei --rich logs
    git-svn-id: http://www.ichebao.net/svn/acb/trunk@2113 fbfde4e3-4bc3-4d81-a63b-7b332075e0cf

--- apps/eventer/handlers/main.py
+++ apps/eventer/handlers/main.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import logging
+import os
 
 from tornado.escape import json_decode
 from constants import EVENTER
@@ -19,14 +20,15 @@ class MainHandler(BaseHandler):
             if body['t'] == EVENTER.INFO_TYPE.POSITION:
                 logging.debug("Get position request from sender:\n%s", body)
                 self.position_queue.put(body)
+                logging.info("Current position queue size:%s, pid:%s", self.position_queue.qsize(), os.getpid())
             elif body['t'] == EVENTER.INFO_TYPE.REPORT:
                 logging.debug("Get report request from sender:\n%s", body)
                 self.report_queue.put(body)
-                logging.info("Current report queue size:%s", self.report_queue.qsize())
+                logging.info("Current report queue size:%s, pid:%s", self.report_queue.qsize(), os.getpid())
             elif body['t'] == EVENTER.INFO_TYPE.CHARGE:
                 logging.debug("Get charge request from sender:\n%s", body)
                 self.position_queue.put(body)
-                logging.info("Current position queue size:%s", self.position_queue.qsize())
+                logging.info("Current position queue size:%s, pid:%s", self.position_queue.qsize(), os.getpid())
             else:
                 logging.warn("Get other request from sender:\n%s", body)
             logging.info("Current position queue size:%s", self.position_queue.qsize())
