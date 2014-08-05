66ea1dec17e1d586de81e70e2834262383633334
jiaxiaolei <jiaxiaolei@fbfde4e3-4bc3-4d81-a63b-7b332075e0cf>
2014-08-05 06:13:26 +0000

    --Jia Xiaolei --rich test_redis.py
    git-svn-id: http://www.ichebao.net/svn/acb/trunk@2110 fbfde4e3-4bc3-4d81-a63b-7b332075e0cf

--- misc/test_redis.py
+++ misc/test_redis.py
@@ -16,53 +16,70 @@ from utils.misc import get_terminal_sessionID_key
 from db_.mysql import DBConnection
 from utils.myredis import MyRedis
 
-def test_redis():
-    db = DBConnection().db
-    redis = MyRedis()
-    tid = 'T123SIMULATOR'
-    sessionID_key = get_terminal_sessionID_key(tid)
-    old_sessionid = redis.get(sessionID_key)
-    print  'session_id: %s' % old_sessionid
 
-def test_mileage():
-    db = DBConnection().db
-    redis = MyRedis()
-    tid = 'CBBJTEAM01'
-    #tid = '361A000066'
-    mileage_key = 'mileage:%s'  % tid
-    #mileage = redis.getvalue(mileage_key)
+class Test(object):
+    def __init__(self):
+        self.db = DBConnection().db
+        self.redis = MyRedis()
 
-    # for hmy
-    #mileage = dict(lat=80547804,
-    #               lon=408846024,
-    #               dis=530, 
-    #               gps_time=1400733148)
-
-    # for lp 
-    mileage = dict(lat=144089748,
-                   lon=418702032,
-                   dis=561, 
-                   gps_time=1400732859)
-    print  'mileage: %s' % mileage 
-    #mileage['dis'] = 530
-    redis.setvalue(mileage_key, mileage)
-
-def test_acc_status():
-    db = DBConnection().db
-    redis = MyRedis()
-    tid = 'T123SIMULATOR'
-    #tid = '13011292218'
-    acc_status_info_key = 'acc_status_info:%s'  % tid
-    acc_status_info = redis.getvalue(acc_status_info_key)
-    print 'tid: %s, acc_status_info_key: %s, acc_status_info: %s' % (tid, 
-           acc_status_info_key, acc_status_info)
+    def test_session(self):
+        tid = 'T123SIMULATOR'
+        sessionID_key = get_terminal_sessionID_key(tid)
+        old_sessionid = self.redis.get(sessionID_key)
+        print  'session_id: %s' % old_sessionid
+    
+    def test_session(self):
+        tid = 'T123SIMULATOR'
+        sessionID_key = get_terminal_sessionID_key(tid)
+        old_sessionid = self.redis.get(sessionID_key)
+        print  'session_id: %s' % old_sessionid
+    
+    def test_mileage(self):
+        tid = '361A000066'
+        mileage_key = 'mileage:%s'  % tid
+        #mileage = self.redis.getvalue(mileage_key)
+    
+        # for hmy
+        #mileage = dict(lat=80547804,
+        #               lon=408846024,
+        #               dis=530, 
+        #               gps_time=1400733148)
+    
+        # for lp 
+        mileage = dict(lat=144089748,
+                       lon=418702032,
+                       dis=561, 
+                       gps_time=1400732859)
+        print  'mileage: %s' % mileage 
+        #mileage['dis'] = 530
+        self.redis.setvalue(mileage_key, mileage)
+    
+    def test_acc_status(self):
+        #tid = '3489722632'
+        tid = '1641486598'
+        acc_status_info_key = 'acc_status_info:%s'  % tid
+        #acc_status_info_key = "acc_status_info:T123SIMULATOR"
+        acc_status_info = self.redis.getvalue(acc_status_info_key)
+        print 'tid: %s, acc_status_info_key: %s, acc_status_info: %s' % (tid, 
+               acc_status_info_key, acc_status_info)
+    
+    def test_alarm_info(self):
+        tid = '1641486598'
+        #tid = 'T123SIMULATOR'
+        alarm_info_key = 'alarm_info:%s' % tid
+        alarm_info = self.redis.getvalue(alarm_info_key)
+        print 'tid: %s, alarm_info_key: %s, alarm_info: %s' % (tid, 
+               alarm_info_key, alarm_info)
 
 def main():
     ConfHelper.load('../conf/global.conf')
     parse_command_line()
 
+    t = Test()
+
     #test_mileage() 
-    test_acc_status() 
+    t.test_acc_status() 
+    #t.test_alarm_info()
 
 if __name__ == "__main__": 
     main()
