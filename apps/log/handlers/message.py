d43f417d78bad55f36e0027ea6005b870291ea5b
jiaxiaolei <jiaxiaolei@fbfde4e3-4bc3-4d81-a63b-7b332075e0cf>
2014-08-09 12:48:59 +0000

    --Jia Xiaolei --add TEST message for USER
    git-svn-id: http://www.ichebao.net/svn/acb/trunk@2126 fbfde4e3-4bc3-4d81-a63b-7b332075e0cf

--- apps/log/handlers/message.py
+++ apps/log/handlers/message.py
@@ -99,6 +99,10 @@ class MessageHandler(BaseHandler):
                     #SMSHelper.send_to_terminal(tmobile,content)
                     SMSHelper.send_update_to_terminal(tmobile,content)
                     self.write_ret(status)              
+                elif sms_type == 'TEST':
+                    content = u'尊敬的顾客，您好：这是一条测试短信，收到本条短信，说明短信提示服务正常，本短信不需要回复，如有问题，请和客服人员联系。感谢使用我们的产品，您的移动卫士。'
+                    SMSHelper.send(tmobile,content)
+                    self.write_ret(status)              
                 elif sms_type == 'DW':
                     content = ':' + sms_type
                     SMSHelper.send_to_terminal(tmobile,content)
