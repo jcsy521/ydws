624cf064152352f7aa15ef3b6609777ef825a6bf
pabb <pabb@fbfde4e3-4bc3-4d81-a63b-7b332075e0cf>
2014-10-13 11:12:04 +0000

    --Jia Xiaolei --add test redis lk
    git-svn-id: http://www.ichebao.net/svn/acb/trunk@2276 fbfde4e3-4bc3-4d81-a63b-7b332075e0cf

---
+++ misc/test_redis_lk.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+
+import sys
+
+import os.path
+import site
+TOP_DIR_ = os.path.abspath(os.path.join(__file__, "../.."))
+site.addsitedir(os.path.join(TOP_DIR_, "libs"))
+import logging
+
+from tornado.options import define, options, parse_command_line
+
+from helpers.confhelper import ConfHelper
+from utils.public import get_terminal_type_by_tid 
+from utils.misc import get_terminal_sessionID_key
+from db_.mysql import DBConnection
+from utils.myredis import MyRedis
+
+# 7 days
+LOCATION_NAME_EXPIRY = 60 * 60 * 24 *7
+
+
+class Test(object):
+
+    def __init__(self):
+        self.db = DBConnection().db
+        self.redis = MyRedis()
+
+    def test_lk(self):
+        #pattern = "lk:4083025:798780"
+        pattern = "lk:*"
+        keys = self.redis.keys(pattern)
+        print 'len ----', len(keys)
+        for k in keys:
+            v = self.redis.get(k)
+            print '-----------k', k, v
+            #self.redis.set(k, v, LOCATION_NAME_EXPIRY)
+
+def main():
+    ConfHelper.load('../conf/global.conf')
+    parse_command_line()
+
+    t = Test()
+    t.test_lk() 
+
+if __name__ == "__main__": 
+    main()
