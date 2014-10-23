4621cb6c28811781a2bf344c0a25331284669064
jiaxiaolei <jiaxiaolei@fbfde4e3-4bc3-4d81-a63b-7b332075e0cf>
2014-10-23 13:39:32 +0000

    --Jia Xiaolei --modify the log
    git-svn-id: http://www.ichebao.net/svn/acb/trunk@2335 fbfde4e3-4bc3-4d81-a63b-7b332075e0cf

--- apps/eventer/handlers/main.py
+++ apps/eventer/handlers/main.py
@@ -20,18 +20,18 @@ class MainHandler(BaseHandler):
             if body['t'] == EVENTER.INFO_TYPE.POSITION:
                 logging.debug("Get position request from sender:\n%s", body)
                 self.position_queue.put(body)
-                logging.info("Current position queue size:%s, pid:%s", self.position_queue.qsize(), os.getpid())
+                logging.info("Current position_queue size:%s, pid:%s", self.position_queue.qsize(), os.getpid())
             elif body['t'] == EVENTER.INFO_TYPE.REPORT:
                 logging.debug("Get report request from sender:\n%s", body)
                 self.report_queue.put(body)
-                logging.info("Current report queue size:%s, pid:%s", self.report_queue.qsize(), os.getpid())
+                logging.info("Current report_queue size:%s, pid:%s", self.report_queue.qsize(), os.getpid())
             elif body['t'] == EVENTER.INFO_TYPE.CHARGE:
                 logging.debug("Get charge request from sender:\n%s", body)
                 self.position_queue.put(body)
-                logging.info("Current position queue size:%s, pid:%s", self.position_queue.qsize(), os.getpid())
+                logging.info("Current position_queue size:%s, pid:%s", self.position_queue.qsize(), os.getpid())
             else:
                 logging.warn("Get other request from sender:\n%s", body)
-            logging.info("Current position queue size:%s", self.position_queue.qsize())
+            logging.info("Current position_queue size:%s", self.position_queue.qsize())
         except:
             logging.exception("[EVENTER] what's up")
 
