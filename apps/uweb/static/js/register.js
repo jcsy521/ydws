717131a679bfb9e748baaa695c3e183c7db479d3
jiaxiaolei <jiaxiaolei@fbfde4e3-4bc3-4d81-a63b-7b332075e0cf>
2014-11-20 07:30:45 +0000

    --hs --优化注册时的消息处理
    git-svn-id: http://www.ichebao.net/svn/acb/trunk@2394 fbfde4e3-4bc3-4d81-a63b-7b332075e0cf

--- apps/uweb/static/js/register.js
+++ apps/uweb/static/js/register.js
@@ -15,19 +15,21 @@ $(function() {
 			n_valLength = str_tmobile.length;
 		
 		if ( n_valLength > 14 || n_valLength < 11 ) {
-			dlf.fn_jNotifyMessage('定位器手机号输入不合法，请重新输入！', 'message', false, 3000);
+			dlf.fn_jNotifyMessage('定位器手机号输入不合法，请重新输入！', 'error', false, 3000);
 			return;
 		} else {
 			if ( !MOBILEREG.test(str_tmobile) ) {	// 手机号合法性验证
-				dlf.fn_jNotifyMessage('定位器手机号格式不正确，请重新输入！', 'message', false, 3000);
+				dlf.fn_jNotifyMessage('定位器手机号格式不正确，请重新输入！', 'error', false, 3000);
 				return;
 			}
 		}
 		$.get_('/checktmobile' + '/' + str_tmobile, '', function(data){
 			if ( data.status != 0 ) {
-				dlf.fn_jNotifyMessage(data.message, 'message', false, 5000);
+				dlf.fn_jNotifyMessage(data.message, 'error', false, 5000);
+				$('#txt_tmobile').data({'errormsg': data.message, 'isvalid': false});
 				return;
 			}
+			$('#txt_tmobile').data({'isvalid': true});
 			dlf.fn_closeJNotifyMsg('#jNotifyMessage');
 		}, 
 		function (XMLHttpRequest, textStatus, errorThrown) {
@@ -40,7 +42,7 @@ $(function() {
 		wideWord: false, // 一个汉字当一个字节
 		submitButtonID: 'btn_register', // 指定本form的submit按钮
 		onError: function(msg, ea) {
-			dlf.fn_jNotifyMessage(msg, 'message', false, 4000);
+			dlf.fn_jNotifyMessage(msg, 'error', false, 4000);
 			$(ea).addClass('borderRed');
 		}, 
 		onSuccess: function() { 
@@ -86,10 +88,12 @@ $(function() {
 	// 获取验证码
 	$('#btnCaptcha').unbind('click').bind('click', function() {
 		var obj_mobile = $('#txt_umobile'),
+			str_tmobile = $.trim($('#txt_tmobile').val()),
 			obj_captchaBtn = $('#btnCaptcha'),
-			str_mobile = obj_mobile.val(),
+			str_mobile = $.trim(obj_mobile.val()),
 			obj_captcha = $('#captcha'),
 			b_flag  = $('#mobileTip .onError').is(":visible"),
+			b_tmobile = $('#txt_tmobile').data('isvalid');
 			n_seconds = parseInt($('#flashTimeText').html());
 		
 		obj_mobile.parent().addClass('focus');
@@ -101,30 +105,46 @@ $(function() {
 		} else {
 			obj_captchaBtn.removeAttr('disabled');
 		}
-		
 		if ( str_mobile == '' ) {  
 			dlf.fn_jNotifyMessage('请输入用户手机号！', 'error', false, 3000);
 			obj_mobile.focus();
+			return;
 		} else if ( !MOBILEREG.test(str_mobile) ) {	// 手机号合法性验证
 			dlf.fn_jNotifyMessage('您输入的号码不合法，请重新输入！', 'error', false, 3000);
 			obj_mobile.focus();
-		} else {
-			$.get_('/register?umobile='+str_mobile, '', function(data) {
-				if ( data.status == 0 ) {
-					//  倒计时1分钟 同时 按钮不可用
-					dlf.fn_jNotifyMessage('验证码发送到您的手机，请在5分钟内激活。', 'message', false, 5000);
-					fn_resetUpdateTime();	// 重新读秒操作
-					$('#btnCaptcha').attr('disabled',true);
-					obj_captcha.parent().addClass('focus');
-					obj_captcha.focus();
-				} else {
-					dlf.fn_jNotifyMessage(data.message, 'error', false, 3000);
-				}
-			}, 
-			function (XMLHttpRequest, textStatus, errorThrown) {
-				dlf.fn_serverError(XMLHttpRequest);
-			});
+			return;
 		}
+		
+		if ( str_tmobile.length > 14 || str_tmobile.length < 11 ) {
+			dlf.fn_jNotifyMessage('定位器手机号输入不合法，请重新输入！', 'error', false, 3000);
+			$('#txt_tmobile').focus();
+			return;
+		} else if ( !MOBILEREG.test(str_tmobile) ) {	// 手机号合法性验证
+			dlf.fn_jNotifyMessage('定位器手机号格式不正确，请重新输入！', 'error', false, 3000);
+			$('#txt_tmobile').focus();
+			return;		
+		}else if ( !b_tmobile ) {			
+			dlf.fn_jNotifyMessage($('#txt_tmobile').data('errormsg'), 'error', false, 3000);
+			$('#txt_tmobile').focus();
+			return;
+		}
+		
+		
+		$.get_('/register?umobile='+str_mobile, '', function(data) {
+			if ( data.status == 0 ) {
+				//  倒计时1分钟 同时 按钮不可用
+				dlf.fn_jNotifyMessage('验证码发送到您的手机，请在5分钟内激活。', 'message', false, 5000);
+				fn_resetUpdateTime();	// 重新读秒操作
+				$('#btnCaptcha').attr('disabled',true);
+				obj_captcha.parent().addClass('focus');
+				obj_captcha.focus();
+			} else {
+				dlf.fn_jNotifyMessage(data.message, 'error', false, 3000);
+			}
+		}, 
+		function (XMLHttpRequest, textStatus, errorThrown) {
+			dlf.fn_serverError(XMLHttpRequest);
+		});
 	}).removeAttr('disabled');
 	
 	$('#txt_umobile').formValidator().inputValidator({min: 1, onError: '请输入用户手机号！'}).regexValidator({regExp: 'mobile', dataType: 'enum', onError:'用户手机号格式不正确，请重新输入！'});
