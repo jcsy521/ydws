1ce467357f675c84e41d63bf00fa878d4de69b70
jiaxiaolei <jiaxiaolei@fbfde4e3-4bc3-4d81-a63b-7b332075e0cf>
2014-12-24 06:57:22 +0000

    --Jia xiaolei --tiny modify
    git-svn-id: http://www.ichebao.net/svn/acb/trunk@2468 fbfde4e3-4bc3-4d81-a63b-7b332075e0cf

--- apps/admin/handlers/captcha.py
+++ apps/admin/handlers/captcha.py
@@ -2,7 +2,10 @@
 
 import tornado.web
 
-import Image, ImageDraw, ImageFont, ImageFilter 
+import Image
+import ImageDraw
+import ImageFont
+import ImageFilter
 import random
 import string
 import cStringIO
@@ -26,12 +29,12 @@ class CaptchaHandler(BaseHandler):
         size = (123, 25)
         jam_num = (1, 2)
         point_border = (100, 97)
-        
+
         im = Image.new('RGB', size, (255, 255, 255))
         draw = ImageDraw.Draw(im)
 
         rand_str = ''.join(random.choice(string.letters + string.digits) for x in range(4))
-        
+
         draw.text((random.randint(1, 5), -2),
                   rand_str[0],
                   fill=(0, 0, 255),
@@ -41,19 +44,19 @@ class CaptchaHandler(BaseHandler):
                   fill=(255, 0, 0),
                   font=ImageFont.truetype(self.FONT_FILE, random.randint(18, 20)))
         draw.text((random.randint(55, 65), -2),
-                  rand_str[2], 
+                  rand_str[2],
                   fill=(0, 0, 255),
                   font=ImageFont.truetype(self.FONT_FILE, random.randint(18, 20)))
         draw.text((random.randint(80, 90), -2),
                   rand_str[3],
                   fill=(255, 0, 0),
                   font=ImageFont.truetype(self.FONT_FILE, random.randint(18, 20)))
-        
+
         # noise lines
         line_num = random.randint(jam_num[0], jam_num[1])
         for i in range(line_num):
             begin = (random.randint(0, size[0]), random.randint(0, size[1]))
-            end = (random.randint(0, size[0]), random.randint(0, size[1])) 
+            end = (random.randint(0, size[0]), random.randint(0, size[1]))
             draw.line([begin, end], fill=(0, 0, 0))
 
         # noise points
@@ -64,13 +67,13 @@ class CaptchaHandler(BaseHandler):
                     draw.point((x, y), fill=(0, 0, 0))
                     #del flag
 
-        # # twist the figure 
-        # para = [1 - float(random.randint(1, 2))/100, 
-        #         0, 
-        #         0, 
-        #         0, 
-        #         1 - float(random.randint(1, 10))/100, 
-        #         float(random.randint(1, 2))/500, 
+        # twist the figure
+        # para = [1 - float(random.randint(1, 2))/100,
+        #         0,
+        #         0,
+        #         0,
+        #         1 - float(random.randint(1, 10))/100,
+        #         float(random.randint(1, 2))/500,
         #         0.001,
         #         float(random.randint(1, 2))/500]
         # im = im.transform(im.size, Image.PERSPECTIVE,para)
@@ -80,7 +83,7 @@ class CaptchaHandler(BaseHandler):
         im.save(buf, 'gif')
 
         m = hashlib.md5()
-        m.update(rand_str.lower()) 
+        m.update(rand_str.lower())
         hash_ = m.hexdigest()
 
         self.set_cookie('captchahash', hash_)
